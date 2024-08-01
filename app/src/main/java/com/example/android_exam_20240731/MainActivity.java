960850d62b167d0dcd2ad9dd0915d066ff49e225
이정기 <jk@ubcn.co.kr>
2024-08-01 14:55:52 +0900

    웹뷰 페이지 로드

---
+++ app/src/main/java/com/example/android_exam_20240731/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.android_exam_20240731;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUrl;
+    private WebView webViewMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });*/
+
+        editTextUrl = findViewById(R.id.activity_main__editTextUrl);
+        webViewMain = findViewById(R.id.activity_main__webViewMain);
+
+        // 웹 세팅 객체 호출
+        WebSettings websettings = webViewMain.getSettings();
+        // 자바스크립트 엔진활성화
+        websettings.setJavaScriptEnabled(true);
+        webViewMain.setWebViewClient(new WebViewClient());
+        webViewMain.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
+        // 웹 세팅 객체 가져와서
+        WebSettings webSettings = webViewMain.getSettings();
+        // 수정한다.(자바스크립트 엔진 활성화)
+        webSettings.setJavaScriptEnabled(true);
+
+        webViewMain.setWebViewClient(new WebViewClient());
+
+        // 에디터 액션 클릭하면 아래 메서드 실행된다.
+        editTextUrl.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                // 검색
+                String url = editTextUrl.getText().toString().trim();
+
+                if (url.startsWith("http://") == false && url.startsWith("https://") == false) {
+                    url = "http://" + url;
+
+                    editTextUrl.setText(url);
+                }
+
+                goToUrl(url);
+
+                // 키보드 내리기
+                InputMethodManager imm = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                return true;
+            }
+
+            return false;
+        });
+
+        webViewMain.loadUrl("file:///android_asset/index.html");
+    }
+
+    private void goToUrl(String url) {
+        webViewMain.loadUrl(url);
+    }
+
+    public void RefreshButtonClicked(View view) {
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Android_exam_20240731"
+include(":app")
